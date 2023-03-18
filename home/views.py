28410d4acd9e116d1748ad0ae2827f8363b344d4
reshmaissac <reshmaissac2012@gmail.com>
2023-03-18 16:33:40 +0000

    installed django

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home/home.html', {'title': 'Welcome Page'})
+
+def about(request):
+    return render(request, 'home/about.html', {'title': 'About Us'})
+
+def contact(request):
+    return render(request, 'home/contact.html', {'title': 'Contact Us'})
