48bc543357f69169b721e7c1318720f9f4b7a5ac
FogaChokor <fogachokor@yahoo.com>
2023-04-19 10:54:02 +0100

    database reconfiguration

--- djangoReviewApp/settings.py
+++ djangoReviewApp/settings.py
@@ -95,19 +95,14 @@ EMAIL_HOST_PASSWORD = 'tdocrlzgqlgmcteg'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'reviewApp',
+        'NAME': 'review_app',
         'USER': 'root',
-        'PASSWORD': 'sqlpassword1',
-        'HOST': 'localhost',
+        'PASSWORD': 'root',
+        'HOST': '192.158.5.13',
         'PORT': '3306'
     }
 }
