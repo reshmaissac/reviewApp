cdcfdb5e47aa7254fd011b6263ee97eb2866123b
reshmaissac <reshmaissac2012@gmail.com>
2023-04-21 15:30:26 +0000

    added azure account_name and key as environment variables

--- djangoReviewApp/custom_azure.py
+++ djangoReviewApp/custom_azure.py
@@ -1,13 +1,14 @@
+import os
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'reviewappstorage001' # Must be replaced by your <storage_account_name>
-    account_key = 'N1HVJib7A2Bv29unqW8zzh6WFNDeEIkPsGBg5sHw0zvhfjOPmYsrlrIFS++FVdVtelAlUxRqsx63+AStw7gAFA==' # Must be replaced by your <storage_account_key>
+    account_name = os.environ['AZURE_ACCOUNT_NAME']
+    account_key = os.environ['AZURE_ACCOUNT_KEY']
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'reviewappstorage001' # Must be replaced by your storage_account_name
-    account_key = 'N1HVJib7A2Bv29unqW8zzh6WFNDeEIkPsGBg5sHw0zvhfjOPmYsrlrIFS++FVdVtelAlUxRqsx63+AStw7gAFA==' # Must be replaced by your <storage_account_key>
+    account_name = os.environ['AZURE_ACCOUNT_NAME']
+    account_key = os.environ['AZURE_ACCOUNT_KEY']
     azure_container = 'static'
     expiration_secs = None
--- djangoReviewApp/settings.py
+++ djangoReviewApp/settings.py
@@ -181,10 +181,11 @@ LOGIN_URL = 'users:login'
 DEFAULT_FILE_STORAGE = 'djangoReviewApp.custom_azure.AzureMediaStorage'
 STATICFILES_STORAGE = 'djangoReviewApp.custom_azure.AzureStaticStorage'
 
-STATIC_LOCATION = "static"
+STATIC_LOCATION = "static"  
 MEDIA_LOCATION = "media"
 
-AZURE_ACCOUNT_NAME = "reviewappstorage001"
+AZURE_ACCOUNT_NAME = os.environ['AZURE_ACCOUNT_NAME']
+AZURE_ACCOUNT_KEY = os.environ['AZURE_ACCOUNT_KEY']
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
